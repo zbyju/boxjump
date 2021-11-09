cf409fd4782f39c667ef7ce27be1e1a29b7ed8eb
zbyju <zbynek.jurica@gmail.com>
2021-11-09 20:48:30 +0100

    Implement more power when jump held down longer

--- src/utils/player.ts
+++ src/utils/player.ts
@@ -1,5 +1,13 @@
 import { PlayerJump } from "../types/player"
 
+function easeInQuad(x: number): number {
+    return x * x;
+}
+
 export const calculateJumpPower = (jump: PlayerJump): number => {
-    return 1
+    const maxTime = 1
+    const diffMS = Math.min(maxTime, Math.abs(jump.jumpEnd - jump.jumpStart) / 1e3) / maxTime
+    const power = Math.max(0.15, easeInQuad(diffMS))
+    console.log(power)
+    return power
 }
