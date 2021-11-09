c6821af360a33345358535cdda23f05f9c596b81
zbyju <zbynek.jurica@gmail.com>
2021-11-09 17:32:24 +0100

    Add matter.js and implement basic player movement

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,107 @@
 import * as ECS from '../libs/pixi-ecs';
 import * as PIXI from 'pixi.js';
+import Matter, { Body } from 'matter-js';
+import * as PixiMatter from '../libs/pixi-matter'
+import { interactiveTarget } from 'pixi.js';
+
+
+class PlayerController extends ECS.Component {
+	playerBody: Matter.Body
+
+	constructor(playerBody: Matter.Body) {
+		super()
+		this.playerBody = playerBody
+	}
+
+	moveLeft() {
+		Matter.Body.setPosition(this.playerBody, {
+			x: this.playerBody.position.x - 5,
+			y: this.playerBody.position.y
+		})
+	}
+
+	moveRight() {
+		Matter.Body.setPosition(this.playerBody, {
+			x: this.playerBody.position.x + 5,
+			y: this.playerBody.position.y
+		})
+	}
+
+	onUpdate(delta: number, absolute: number) {
+		const keyInputComponent = this.scene.findGlobalComponentByName<ECS.KeyInputComponent>(ECS.KeyInputComponent.name)
+
+		if(keyInputComponent.isKeyPressed(ECS.Keys.KEY_LEFT)) {
+			this.moveLeft()
+		}
+		if(keyInputComponent.isKeyPressed(ECS.Keys.KEY_RIGHT)) {
+			this.moveRight()
+		}
+	}
+}
+
+interface Resolution {
+	width: number,
+	height: number,
+}
 
 class ProjectJumper {
 	engine: ECS.Engine;
+	binder: PixiMatter.MatterBind
 
 	constructor() {
 		this.engine = new ECS.Engine();
-		let canvas = (document.getElementById('gameCanvas') as HTMLCanvasElement);
+		const canvas = (document.getElementById('gameCanvas') as HTMLCanvasElement);
+		this.binder = new PixiMatter.MatterBind();
 
 		this.engine.init(canvas, {
 			resizeToScreen: true,
+			backgroundColor: 0x000000,
+			width: canvas.width,
+			height: canvas.height,
 		});
 
 		this.engine.app.loader
 			.reset()
 			.add('ghost', 'assets/ghost.png')
 			.load(() => this.onAssetsLoaded());
+
+		this.binder.init(this.engine.scene, {
+			mouseControl: false,
+			renderConstraints: true,
+			renderAngles: false
+		})
 	}
 
 	onAssetsLoaded() {
-		let scene = this.engine.scene;
+		let scene: ECS.Scene = this.engine.scene;
+		scene.addGlobalComponent(new ECS.KeyInputComponent())
 
-		Array(100).fill(0, 0, 100).forEach(() => {
-			new ECS.Builder(scene)
-				.localPos(Math.random() * this.engine.app.screen.width, Math.random() * this.engine.app.screen.height)
-				.anchor(0.5)
-				.scale(0.15)
-				.withParent(scene.stage)
-				.withComponent(new ECS.FuncComponent('rotation').doOnUpdate((cmp, delta, absolute) => cmp.owner.rotation += 0.001 * delta))
-				.asSprite(PIXI.Texture.from('ghost'))
-				.build();
-		});
+		this.initBoundry()
+		this.initPlayer();
+	}
 
-		new ECS.Builder(scene)
-			.localPos(this.engine.app.screen.width / 2, this.engine.app.screen.height / 2)
-			.anchor(0.5)
-			.withParent(scene.stage)
-			.withComponent(new ECS.FuncComponent('rotation').doOnUpdate((cmp, delta, absolute) => cmp.owner.rotation += 0.001 * delta))
-			.asText('Hello World', new PIXI.TextStyle({ fill: '#FF0000', fontSize: 80, fontFamily: 'Courier New' }))
-			.build();
+	initBoundry() {
+		const boundryThickness = 25
+		Matter.World.add(this.binder.mWorld, [
+			Matter.Bodies.rectangle(boundryThickness / 2, this.engine.app.screen.height / 2, boundryThickness, this.engine.app.screen.height, { isStatic: true }),
+		]);
+		Matter.World.add(this.binder.mWorld, [
+			Matter.Bodies.rectangle(this.engine.app.screen.width - boundryThickness / 2, this.engine.app.screen.height / 2, boundryThickness, this.engine.app.screen.height, { isStatic: true }),
+		]);
+		Matter.World.add(this.binder.mWorld, [
+			Matter.Bodies.rectangle(this.engine.app.screen.width / 2, this.engine.app.screen.height - boundryThickness / 2, this.engine.app.screen.width, boundryThickness, { isStatic: true }),
+		]);
+	}
+
+	initPlayer() {
+		const playerBody = Matter.Bodies.rectangle(this.engine.app.screen.width / 2, this.engine.app.screen.height - 60, 20, 35)
+		const playerContainer = this.binder.addBody(playerBody)
+
+		playerContainer.addComponent(new PlayerController(playerBody))
+	}
+
+	initLevel() {
+
 	}
 }
 
