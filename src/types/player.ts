257c69eb9452e791a3b7a5ceb10945703ea23413
zbyju <zbynek.jurica@gmail.com>
2021-11-09 20:15:00 +0100

    Add types for player variables

---
+++ src/types/player.ts
@@ -0,0 +1,19 @@
+export enum PlayerState {
+    STANDING,
+    RUNNING_LEFT,
+    RUNNING_RIGHT,
+    JUMPING,
+    FLYING
+}
+
+export enum JumpDirection {
+    UP = 0,
+    LEFT = -1,
+    RIGHT = 1,
+}
+
+export type PlayerJump = {
+    jumpStart: number | null,
+    jumpEnd: number | null,
+    jumpDirection: JumpDirection
+}
