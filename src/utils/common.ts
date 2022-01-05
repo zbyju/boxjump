078754f2392a26d7c15090d536356116d2f292ea
Zbynek Jurica <zbynek.jurica@gmail.com>
2022-01-05 23:32:10 +0100

    Add utils to recalculate coords

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,6 +1,43 @@
+import Matter from 'matter-js';
 import * as ECS from '../../libs/pixi-ecs';
-import { Resolution } from '../types/common';
+import { Coordinates, Resolution, Size } from '../types/common';
 
 export const getResolutionFromEngine = (engine: ECS.Engine): Resolution => {
     return { width: engine.app.screen.width, height: engine.app.screen.height }
+}
+
+export const coordinatesFromCenterToCorner = (coords: Coordinates, size: Size): Coordinates => {
+    return {
+        x: coords.x - size.width / 2,
+        y: coords.y + size.height / 2
+    }
+}
+
+export const coordinatesFromCornerToCenter = (coords: Coordinates, size: Size): Coordinates => {
+    return {
+        x: coords.x + size.width / 2,
+        y: coords.y - size.height / 2
+    }
+}
+
+
+export const coordinatesFromCenterToRightCorner = (coords: Coordinates, size: Size, resolution: Resolution): Coordinates => {
+    return {
+        x: resolution.width - (coords.x - size.width / 2),
+        y: coords.y + size.height / 2
+    }
+}
+
+export const coordinatesFromRightCornerToCenter = (coords: Coordinates, size: Size, resolution: Resolution): Coordinates => {
+    return {
+        x: resolution.width - (coords.x + size.width / 2),
+        y: coords.y - size.height / 2
+    }
+}
+
+export const calculateBoxSize = (box: Matter.Body): Size => {
+    return {
+        width: box.bounds.max.x - box.bounds.min.x,
+        height: box.bounds.max.y - box.bounds.min.y
+    }
 }
