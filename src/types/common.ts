7f6449903718d6d3c47e57de3088a921455463bf
Zbynek Jurica <zbynek.jurica@gmail.com>
2022-01-05 23:32:23 +0100

    Add size, coord types

--- src/types/common.ts
+++ src/types/common.ts
@@ -2,3 +2,13 @@ export type Resolution = {
     width: number,
     height: number
 }
+
+export type Size = {
+    width: number
+    height: number
+}
+
+export type Coordinates = {
+    x: number
+    y: number
+}
