4e65ddf9cf2bbff0bbdea349858fcfaf98d575be
Zbynek Jurica <zbynek.jurica@gmail.com>
2022-01-06 21:09:53 +0100

    Add level 4

--- src/levels/level4.ts
+++ src/levels/level4.ts
@@ -5,10 +5,9 @@ import { BoxRightFactory } from "../factory/boxRightFactory";
 import { Box } from "../objects/box";
 import { Resolution } from "../types/common";
 import { Level } from "./level";
-import { Level2 } from "./level2";
-import { Level4 } from "./level4";
+import { Level3 } from "./level3";
 
-export class Level3 extends Level {
+export class Level4 extends Level {
     boxes: Box[];
     name: string = "2"
     playerStart: Resolution;
@@ -16,30 +15,30 @@ export class Level3 extends Level {
 
     constructor(resolution: Resolution) {
         super(resolution)
-        this.playerStart = {width: this.resolution.width / 2, height: this.resolution.height - 25}
+        this.playerStart = {width: 50, height: this.resolution.height - 50}
         this.playerEnd = {width: this.resolution.width / 3 - 20, height: this.resolution.height / 6}
 
         const bfl = new BoxLeftFactory(this.resolution)
         const bfr = new BoxRightFactory(this.resolution)
         const bfc = new BoxCenterFactory(this.resolution)
         this.boxes = [
-            bfc.createMDBox(this.resolution.width / 2, this.resolution.height - 10),
+            bfl.createBox(-2, 0, 4, 1000),
+            bfr.createBox(-2, 0, 4, 1000),
+            bfl.createMDBox(0, this.resolution.height - 10),
+            bfl.createXSBox(150, this.resolution.height - 200),
+            bfl.createXSBox(400, this.resolution.height - 280),
+            bfl.createXSBox(650, this.resolution.height - 360),
 
-            bfl.createSMBox(200, this.resolution.height - 170),
-            bfl.createXSBox(100, this.resolution.height - 220),
-            bfl.createSMBox(300, this.resolution.height - 280),
-            bfl.createSMBox(200, this.resolution.height - 450),
-
-            bfr.createXSBox(100, this.resolution.height - 370)
+            bfl.createBox(0, this.resolution.height - 400, 300, 30),
         ]
     }
 
     getNextLevel(): Level | null {
-        return new Level4(this.resolution)
+        return null
     }
 
     getPrevLevel(): Level | null {
-        return new Level2(this.resolution)
+        return new Level3(this.resolution)
     }
 
 }
