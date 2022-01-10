2b7a269737b0ca5630b6c3e9c194ec0ad365c04b
Zbynek Jurica <zbynek.jurica@gmail.com>
2022-01-10 16:57:36 +0100

    Add keyboard controller

---
+++ src/controllers/keyboardController.ts
@@ -0,0 +1,45 @@
+import * as ECS from '../../libs/pixi-ecs';
+
+export class KeyboardController {
+    keyInputComponent: ECS.KeyInputComponent
+
+    constructor(keyInputComponent: ECS.KeyInputComponent) {
+        this.keyInputComponent = keyInputComponent
+    }
+
+    shouldMoveRight() {
+        return this.keyInputComponent.isKeyPressed(ECS.Keys.KEY_RIGHT)
+    }
+
+    shouldMoveLeft() {
+        return this.keyInputComponent.isKeyPressed(ECS.Keys.KEY_LEFT)
+    }
+
+    shouldJump() {
+        return this.keyInputComponent.isKeyPressed(ECS.Keys.KEY_SPACE)
+    }
+
+    shouldJumpRight() {
+        return !this.shouldMoveLeft() && this.shouldMoveRight()
+    }
+
+    shouldJumpLeft() {
+        return this.shouldMoveLeft() && !this.shouldMoveRight()
+    }
+
+    shouldTeleportNextLevel() {
+        return this.keyInputComponent.isKeyPressed(ECS.Keys.KEY_O)
+    }
+
+    shouldTeleportPreviousLevel() {
+        return this.keyInputComponent.isKeyPressed(ECS.Keys.KEY_K)
+    }
+
+    shouldTeleportEnd() {
+        return this.keyInputComponent.isKeyPressed(ECS.Keys.KEY_P)
+    }
+
+    shouldTeleportStart() {
+        return this.keyInputComponent.isKeyPressed(ECS.Keys.KEY_L)
+    }
+}
